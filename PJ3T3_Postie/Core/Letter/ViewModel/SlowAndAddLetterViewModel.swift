603bdc0ce0c3d362dc11c4af6ca11b0f7698327e
Eunice <jes.e.0927@gmail.com>
2024-05-20 23:55:57 +0900

    Refactor: SlowPostboxViewModel과 AddLetterViewModel의 공통 요소

--- PJ3T3_Postie/Core/Letter/ViewModel/SlowAndAddLetterViewModel.swift
+++ PJ3T3_Postie/Core/Letter/ViewModel/SlowAndAddLetterViewModel.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+
+class SlowAndAddItemViewModel: CommonLetterViewModel {
+    @Published var showingDismissAlert: Bool = false
+    @Published var showingNotEnoughInfoAlert: Bool = false
+    @Published var showingUploadErrorAlert: Bool = false
+    
+    var isReceived: Bool
+    
+    init(isReceived: Bool) {
+        self.isReceived = isReceived
+    }
+    
+    func showNotEnoughInfoAlert() {
+        showingNotEnoughInfoAlert = true
+    }
+    
+    func showUploadErrorAlert() {
+        showingUploadErrorAlert = true
+    }
+    
+    func showDismissAlert() {
+        showingDismissAlert = true
+    }
+    
+    func uploadImages(docId: String) async throws -> ([String], [String]) {
+        var newImageFullPaths = [String]()
+        var newImageUrls = [String]()
+
+        // 이미지 추가
+        for image in images {
+            let fullPath = try await StorageManager.shared.uploadUIImage(image: image, docId: docId)
+            let url = try await StorageManager.shared.requestImageURL(fullPath: fullPath)
+            newImageFullPaths.append(fullPath)
+            newImageUrls.append(url)
+        }
+
+        return (newImageFullPaths, newImageUrls)
+    }
+}
