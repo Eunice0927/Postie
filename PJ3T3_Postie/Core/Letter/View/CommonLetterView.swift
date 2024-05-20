8b106b222c27f16d0bf087e069905aab0b9fa28e
Eunice <jes.e.0927@gmail.com>
2024-05-20 23:18:14 +0900

    Add: CommonLetterView와 ViewModel 파일 생성

---
+++ PJ3T3_Postie/Core/Letter/View/CommonLetterView.swift
@@ -0,0 +1,18 @@
+//
+//  CommonLetterView.swift
+//  PJ3T3_Postie
+//
+//  Created by Eunsu JEONG on 5/20/24.
+//
+
+import SwiftUI
+
+struct CommonLetterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CommonLetterView()
+}
