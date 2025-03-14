70bd8c190c51db8874635c491f8e91c242220c82
burst-cannon <qlrmr111@naver.com>
2025-03-15 06:58:22 +0900

    Refactor: NoticeView 리팩토링

--- PJ3T3_Postie/Core/Setting/View/NoticeView.swift
+++ PJ3T3_Postie/Core/Setting/View/NoticeView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct NoticeView: View {
     @ObservedObject var firestoreNoticeManager = FirestoreNoticeManager.shared
-    @StateObject private var viewModel = NoticeViewModel()
+    @StateObject private var noticeViewModel = NoticeViewModel()
     
     var body: some View {
         ZStack {
@@ -24,7 +24,7 @@ struct NoticeView: View {
                     
                     if firestoreNoticeManager.notices.isEmpty {
                         VStack {
-                            Image(viewModel.isThemeGroupButton == 4 ? "postyThinkingSketchWhite" : "postyThinkingSketch")
+                            Image(noticeViewModel.isThemeGroupButton == 4 ? "postyThinkingSketchWhite" : "postyThinkingSketch")
                                 .resizable()
                                 .scaledToFit()
                                 .frame(height: 200)
@@ -58,7 +58,7 @@ struct NoticeView: View {
 //                                            .scaledToFit()
 //                                    }
                                     
-                                    let noticeText = viewModel.parseText(notice.content.replacingOccurrences(of: "\\n", with: "\n"))
+                                    let noticeText = noticeViewModel.parseText(notice.content.replacingOccurrences(of: "\\n", with: "\n"))
                                     
                                     ForEach(noticeText, id: \.0) { part in
                                         Text(part.0)
